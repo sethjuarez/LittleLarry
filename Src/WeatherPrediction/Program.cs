194ed70d486ce2bc983b3a21e5a84a82e1650830
sethjuarez <me@sethjuarez.com>
2017-05-22 11:30:01 +0200

    added basic ml project

---
+++ Src/WeatherPrediction/Program.cs
@@ -0,0 +1,40 @@
+﻿using numl.Model;
+using numl.Supervised.DecisionTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var descriptor = Descriptor.For<Tennis>()
+                                       .With(t => t.Outlook)
+                                       .With(t => t.Temperature)
+                                       .With(t => t.Windy)
+                                       .With(t => t.Humidity)
+                                       .Learn(t => t.Play);
+
+            Console.WriteLine(descriptor);
+
+            var generator = new DecisionTreeGenerator(descriptor);
+            var model = generator.Generate(Tennis.GetData());
+            Console.WriteLine($"Model:\n{model}");
+
+            var tn = new Tennis { Outlook = Outlook.Sunny,
+                                 Temperature = Temperature.Cool,
+                                 Humidity = Humidity.High,
+                                 Windy = true };
+
+            var tennis = model.PredictValue(tn);
+
+            Console.WriteLine($"Play? {tennis}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Src/WeatherPrediction/Tennis.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using numl.Model;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WeatherPrediction
+{
+    public enum Outlook
+    {
+        Sunny,
+        Overcast,
+        Rainy
+    }
+
+    public enum Temperature
+    {
+        Hot,
+        Mild,
+        Cool
+    }
+
+    public enum Humidity
+    {
+        High,
+        Normal
+    }
+
+    public class Tennis
+    {
+        public Outlook Outlook { get; set; }
+        public Temperature Temperature { get; set; }
+        public Humidity Humidity { get; set; }
+        public bool Windy { get; set; }
+        public bool Play { get; set; }
+
+        public static Tennis Make(Outlook outlook, Temperature temperature, Humidity humidity, bool windy, bool play)
+        {
+            return new Tennis
+            {
+                Outlook = outlook,
+                Temperature = temperature,
+                Humidity = humidity,
+                Windy = windy,
+                Play = play
+            };
+        }
+
+        public static Tennis[] GetData()
+        {
+            return new Tennis[]
+            {
+                Tennis.Make(Outlook.Sunny, Temperature.Hot, Humidity.High, false, false),
+                Tennis.Make(Outlook.Sunny, Temperature.Hot, Humidity.High, true,  false),
+                Tennis.Make(Outlook.Overcast, Temperature.Hot, Humidity.High, false, true),
+                Tennis.Make(Outlook.Rainy, Temperature.Mild, Humidity.High, false, true),
+                Tennis.Make(Outlook.Rainy, Temperature.Cool, Humidity.Normal, false, true),
+                Tennis.Make(Outlook.Rainy, Temperature.Cool, Humidity.Normal, true, false),
+                Tennis.Make(Outlook.Overcast, Temperature.Cool, Humidity.Normal, true, true),
+                Tennis.Make(Outlook.Sunny, Temperature.Mild, Humidity.High, false, false),
+                Tennis.Make(Outlook.Sunny, Temperature.Cool, Humidity.Normal, false, true),
+                Tennis.Make(Outlook.Rainy, Temperature.Mild, Humidity.Normal, false, true),
+                Tennis.Make(Outlook.Sunny, Temperature.Mild, Humidity.Normal, true, true),
+                Tennis.Make(Outlook.Overcast, Temperature.Mild, Humidity.High, true, true),
+                Tennis.Make(Outlook.Overcast, Temperature.Hot, Humidity.Normal, false, true),
+                Tennis.Make(Outlook.Rainy, Temperature.Mild, Humidity.High, true, false)
+            };
+        }
+    }
+}
